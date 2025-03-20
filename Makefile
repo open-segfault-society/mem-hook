f0808ee20780b11ab5f543c195122a060a1b467b
Dethcrvsh <fabianfabbe@hotmail.se>
2025-03-20 13:16:41 +0100

    libbacktrace

--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
 # Variables
 CXX = g++
-CXXFLAGS = -fPIC -shared -Wall
+CXXFLAGS = -fPIC -shared -Wall # -g -rdynamic Added flags for debugging and symbol resolution
 LIB_NAME = hook.so
-SRC = $(wildcard hook_lib/*.cpp)  
+SRC = $(wildcard hook_lib/*.cpp)
 OBJ = $(SRC:.cpp=.o)
+
+# libbacktrace settings
+BACKTRACE_LIB = -lbacktrace
 
 # Default target: Build the shared object
 all: $(LIB_NAME)
 
 # Rule to build the shared object
 $(LIB_NAME): $(OBJ)
-	$(CXX) $(CXXFLAGS) -o $@ $^
+	$(CXX) $(CXXFLAGS) -o $@ $^ $(BACKTRACE_LIB)
 
 # Rule to compile the .cpp source files into object files (.o)
 %.o: src/%.cpp  # Ensure object files are compiled from the correct source directory
@@ -21,3 +24,4 @@ clean:
 	rm -f $(OBJ) $(LIB_NAME)
 
 .PHONY: all clean
+
--- hook_lib/hook.cpp
+++ hook_lib/hook.cpp
@@ -1,64 +1,98 @@
 #include "shared_buffer.h"
+#include <backtrace.h>
 #include <chrono>
 #include <cstdlib>
 #include <cstring>
 #include <dlfcn.h>
-#include <execinfo.h>
 #include <iostream>
 
 SharedBuffer buffer{};
+static void error_callback(void* data, const char* msg, int errnum);
+void print_backtrace();
+
+backtrace_state* const state{
+    backtrace_create_state(nullptr, 1, error_callback, nullptr)};
 
 // Define a function pointer for the original functions
-void *(*malloc_real)(size_t) = nullptr;
-void (*free_real)(void *) = nullptr;
+void* (*malloc_real)(size_t) = nullptr;
+void (*free_real)(void*) = nullptr;
 
 // Backtrace related values
-void *backtrace_buffer[BUFFER_SIZE];
+void* backtrace_buffer[BUFFER_SIZE];
 
 // The hook function for malloc
-extern "C" void *malloc_hook(uint32_t size) {
-  void *const ptr{malloc_real(size)}; // Call the original malloc
+extern "C" void* malloc_hook(uint32_t size) {
+    void* const ptr{malloc_real(size)}; // Call the original malloc
 
-  // Get allocation time
-  auto now = std::chrono::steady_clock::now();
-  uint32_t now_ns = std::chrono::duration_cast<std::chrono::milliseconds>(
-                        now.time_since_epoch())
-                        .count();
+    // Get allocation time
+    // auto now = std::chrono::steady_clock::now();
+    // uint32_t now_ns = std::chrono::duration_cast<std::chrono::milliseconds>(
+    //                       now.time_since_epoch())
+    //                       .count();
 
-  // Get call stack address and number of addresses
-  // TODO: test overhead difference of backtrace and backtrace_symbols
-  uint32_t backtrace_size = backtrace(backtrace_buffer, BUFFER_SIZE);
-  Allocation alloc{ptr, size, now_ns, backtrace_size, backtrace_buffer};
+    print_backtrace();
 
-  buffer.write(alloc);
-
-  return ptr;
+    return ptr;
 }
 
-extern "C" void free_hook(void *ptr) {
-  buffer.write(ptr);
-  return free_real(ptr);
-}
+extern "C" void free_hook(void* ptr) { return free_real(ptr); }
 
 // A function to set the original malloc symbol
 void set_original_malloc() {
-  malloc_real = (void *(*)(size_t))dlsym(RTLD_NEXT, "malloc");
-  if (!malloc_real) {
-    std::cerr << "Failed to find original malloc: " << dlerror() << std::endl;
-    exit(1);
-  }
+    malloc_real = (void* (*)(size_t))dlsym(RTLD_NEXT, "malloc");
+    if (!malloc_real) {
+        std::cerr << "Failed to find original malloc: " << dlerror()
+                  << std::endl;
+        exit(1);
+    }
 }
 
 void set_original_free() {
-  free_real = (void (*)(void *))dlsym(RTLD_NEXT, "free");
-  if (!free_real) {
-    std::cerr << "Failed to find original free: " << dlerror() << std::endl;
-    exit(1);
-  }
+    free_real = (void (*)(void*))dlsym(RTLD_NEXT, "free");
+    if (!free_real) {
+        std::cerr << "Failed to find original free: " << dlerror() << std::endl;
+        exit(1);
+    }
+}
+
+/* BACKTRACE TESTING */
+
+struct TraceHelper {
+    size_t frame_count{0};
+    size_t max_frames{10};
+    uintptr_t frames[10];
+};
+
+static void error_callback(void* data, const char* msg, int errnum) {
+    std::cerr << "Error in backtrace: " << msg << "(error " << errnum << ")"
+              << std::endl;
+}
+
+static int simple_callback(void* data, uintptr_t pc) {
+    TraceHelper* helper{reinterpret_cast<TraceHelper*>(data)};
+    helper->frames[helper->frame_count] = pc;
+    helper->frame_count++;
+
+    if (helper->frame_count >= helper->max_frames) {
+        return 1;
+    }
+    return 0;
+}
+
+void print_backtrace() {
+    if (!state) {
+        std::cerr << "Failed to initialize libbacktrace" << std::endl;
+        return;
+    }
+
+    TraceHelper helper{};
+
+    backtrace_simple(state, 0, simple_callback, error_callback,
+                     reinterpret_cast<void*>(&helper));
 }
 
 // Entry point for the shared library
 __attribute__((constructor)) void initialize() {
-  set_original_malloc();
-  set_original_free();
+    set_original_malloc();
+    set_original_free();
 }
