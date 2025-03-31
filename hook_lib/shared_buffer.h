bdbffa03be8db0ff6a57e605e16cbb5c79a0a6d3
Martin Högstedt <marin.hogstedt@hotmail.com>
2025-03-31 12:52:44 +0200

    started to work on seperating the different hooks shared memory through a buffer class

--- hook_lib/shared_buffer.h
+++ hook_lib/shared_buffer.h
@@ -7,57 +7,81 @@
 uint32_t static const BUFFER_SIZE{20};
 
 struct Allocation {
-  void *address;                           // Address returned by malloc
-  uint32_t size;                           // Size of allocation
-  uint32_t time;                           // Time of allocation
-  uint32_t backtrace_size;                 // Backtrace size of allocation
-  std::array<void *, 20> backtrace_buffer; // Actual backtrace
+    void* address;                          // Address returned by malloc
+    uint32_t size;                          // Size of allocation
+    uint32_t time;                          // Time of allocation
+    uint32_t backtrace_size;                // Backtrace size of allocation
+    std::array<void*, 20> backtrace_buffer; // Actual backtrace
 
-  Allocation(void *alloc_address, uint32_t size, uint32_t time,
-             uint32_t backtrace_size, void *(&buffer)[BUFFER_SIZE]);
+    Allocation(void* alloc_address, uint32_t size, uint32_t time,
+               uint32_t backtrace_size, void* (&buffer)[BUFFER_SIZE]);
 };
 
 struct Free {
-  void *address;                           // Address returned by malloc
-  uint32_t time;                           // Time of allocation
-  uint32_t backtrace_size;                 // Backtrace size of allocation
-  std::array<void *, 20> backtrace_buffer; // Actual backtrace
+    void* address;                          // Address returned by malloc
+    uint32_t time;                          // Time of allocation
+    uint32_t backtrace_size;                // Backtrace size of allocation
+    std::array<void*, 20> backtrace_buffer; // Actual backtrace
 
-  Free(void *free_ptr, uint32_t time, uint32_t backtrace_size,
-       void *(&buffer)[BUFFER_SIZE]);
+    Free(void* free_ptr, uint32_t time, uint32_t backtrace_size,
+         void* (&buffer)[BUFFER_SIZE]);
+};
+
+class Buffer {
+  public:
+    Buffer(std::string mount_point, uint32_t num_allocations,
+           uint32_t head_size, uint32_t data_size, uint32_t buffer_size);
+    Buffer();
+
+  private:
+    std::string mount_point;
+    uint32_t num_allocations;
+    uint32_t head_size;
+    uint32_t data_size;
+    uint32_t buffer_size;
+
+    // Shared memory
+    void* memory;
+    int fd;
+
+    // Ring buffer
+    uint32_t* head;
+    uint32_t* tail;
+    char* data_start; // char pointer to avoid dividing memory address with 4
 };
 
 class SharedBuffer {
-public:
-  SharedBuffer();
-  ~SharedBuffer();
-  void write(Allocation const &alloc); // Malloc
-  void write(Free const &free);        // Free
+  public:
+    SharedBuffer();
+    ~SharedBuffer();
+    void write(Allocation const& alloc); // Malloc
+    void write(Free const& free);        // Free
 
-private:
-  char constexpr static ALLOC_MOUNT[] = "/mem_hook_alloc";
-  char constexpr static FREE_MOUNT[] = "/mem_hook_free";
-  uint32_t static const NUM_ALLOCATIONS{1000};
-  uint32_t static const HEAD_SIZE{8};
-  uint32_t static const MALLOC_DATA_SIZE{NUM_ALLOCATIONS *
-                                         sizeof(struct Allocation)};
-  uint32_t static const MALLOC_BUFF_SIZE{HEAD_SIZE + MALLOC_DATA_SIZE};
+  private:
+    Buffer malloc_buffer;
+    Buffer free_buffer;
+    char constexpr static ALLOC_MOUNT[] = "/mem_hook_alloc";
+    char constexpr static FREE_MOUNT[] = "/mem_hook_free";
+    uint32_t static const NUM_ALLOCATIONS{1000};
+    uint32_t static const HEAD_SIZE{8};
+    uint32_t static const MALLOC_DATA_SIZE{NUM_ALLOCATIONS *
+                                           sizeof(struct Allocation)};
+    uint32_t static const MALLOC_BUFF_SIZE{HEAD_SIZE + MALLOC_DATA_SIZE};
 
-  uint32_t static const FREE_DATA_SIZE{NUM_ALLOCATIONS * sizeof(struct Free)};
-  uint32_t static const FREE_BUFF_SIZE{HEAD_SIZE + FREE_DATA_SIZE};
+    uint32_t static const FREE_DATA_SIZE{NUM_ALLOCATIONS * sizeof(struct Free)};
+    uint32_t static const FREE_BUFF_SIZE{HEAD_SIZE + FREE_DATA_SIZE};
 
-  // Shared buffer
-  void *malloc_memory;
-  void *free_memory;
-  int fd_malloc;
-  int fd_free;
+    // Shared buffer
+    void* malloc_memory;
+    void* free_memory;
+    int fd_malloc;
+    int fd_free;
 
-  // Ring buffer
-  uint32_t *malloc_head;
-  uint32_t *malloc_tail;
-  uint32_t *free_head;
-  uint32_t *free_tail;
-  // char pointer to avoid dividing memory address with 4
-  char *malloc_data_start;
-  char *free_data_start;
+    // Ring buffer
+    uint32_t* malloc_head;
+    uint32_t* malloc_tail;
+    uint32_t* free_head;
+    uint32_t* free_tail;
+    char* malloc_data_start;
+    char* free_data_start;
 };
