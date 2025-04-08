002f42ae75057a878a256f8ed2f23442f2d37c5f
Dethcrvsh <fabianfabbe@hotmail.se>
2025-04-08 16:30:51 +0200

    Fixed backtrace hack

--- hook_lib/backtrace.h
+++ hook_lib/backtrace.h
@@ -11,25 +11,25 @@
  * The `skip` parameter specifies how many initial frames to ignore.
  * 
  * @param skip The number of initial stack frames to skip. Typically set to 1 or more 
- *             to exclude `get_stack()` itself from the results.
+ *             to exclude `walk_stack_fp()` itself from the results.
  *
- * @return std::array<uintptr_t, N> An array containing the collected stack frame addresses.
+ * @return std::array<T, N> An array containing the collected stack frame addresses.
  *                                  Each pointer represents the return address of a function call.
  * 
  * @note This function relies on frame pointers being present. Ensure compilation uses
  *       `-fno-omit-frame-pointer` for accurate results.
  * @warning Results may be unreliable if the program is heavily optimized or lacks frame pointers.
  */
-template <std::size_t N>
-inline size_t walk_stack_fp(std::array<uintptr_t, N>& buffer, std::size_t skip) {
-    uintptr_t* fp = reinterpret_cast<uintptr_t*>(__builtin_frame_address(0));
+template <typename T, std::size_t N>
+inline size_t walk_stack_fp(std::array<T, N>& buffer, std::size_t skip) {
+    T* fp = reinterpret_cast<T*>(__builtin_frame_address(0));
     std::size_t i;
 
     for (i = 0; fp && i < N; ++i) {
         if (i >= skip) {
             buffer[i] = fp[1]; // The return address of the current frame
         }
-        fp = reinterpret_cast<uintptr_t*>(fp[0]); // Follow the frame pointer chain
+        fp = reinterpret_cast<T*>(fp[0]); // Follow the frame pointer chain
 
         if (!fp) {
             break;
@@ -37,18 +37,5 @@ inline size_t walk_stack_fp(std::array<uintptr_t, N>& buffer, std::size_t skip) 
     }
 
     return i;
-}
-
-template <typename T, std::size_t N>
-inline size_t walk_stack_fp(std::array<T, N>& buffer, std::size_t skip) {
-    std::array<uintptr_t, 20> temp {};
-    size_t const size {walk_stack_fp<20>(temp, skip)};
-    std::transform(
-        temp.begin(),
-        temp.end(),
-        buffer.begin(),
-        [](uintptr_t ptr) { return reinterpret_cast<T>(ptr); }
-    );
-    return size;
 }
 
